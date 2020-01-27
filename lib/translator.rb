eaf734ea3fabc400b13bced4f45b52f2c6d48753
Mike Pottebaum <mpottebaum@gmail.com>
2020-01-27 22:20:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,7 +22,7 @@ end
 
 def get_japanese_emoticon(file, eng_emoticon)
   library = load_library(file)
-  japanese_emoticon = library[:get_emoticon].fetch(eng_emoticon)
+  japanese_emoticon = library[:get_emoticon].fetch(eng_emoticon, "Sorry, this is not a known emoticon.")
 end
 
 def get_english_meaning
